e09be6b81e3801a28a00d6391f37c69278dc09f0
Djordje Krejakovic <djkrejakovic7722rn@raf.rs>
2026-03-12 00:03:07 +0100

    Inicijalizacija projekta

---
+++ src/pages/EditEmployeePage.jsx
@@ -0,0 +1,3 @@
+export default function EditEmployeePage() {
+  return <h1>Edit Employee Page</h1>;
+}
---
+++ src/pages/EmployeeDetailsPage.jsx
@@ -0,0 +1,3 @@
+export default function EmployeeDetailsPage() {
+  return <h1>Employee Details Page</h1>;
+}
